9e54e48f362388cdba542a386379978f4e7f02f8
WANGMinsi <90818691+WANGMinsi@users.noreply.github.com>
2022-11-24 22:07:45 +0800

    Add files via upload

--- src/Test/Server.java
+++ src/Test/Server.java
@@ -1,144 +1,46 @@
-package Test;
-
-import java.io.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Collection;
-import java.util.HashMap;
-//import java.util.ArrayList;
 
 public class Server {
-    private HashMap<Integer, UserSocket> userMap;
-    private int count = 0;
-    public Server() {
-        userMap = new HashMap<>();
+    public Server(int port) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(port);
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            Thread t = new Thread(() -> {
+                try {
+                    serve(clientSocket);
+                } catch (IOException ex) {
+                    System.out.println("Connection drop!");
+                }
+            });
+            t.start();
+        }
     }
 
-    public void print(String str, Object... o) {
-        System.out.printf(str, o);
-    }
-
-    public void start() throws IOException {
-        ServerSocket serverSocket = new ServerSocket(12345);
-        System.out.println("Server start");
+    private void serve(Socket clientSocket) throws IOException {
+        byte[] buffer = new byte[1024];
+        DataInputStream in = new DataInputStream(clientSocket.getInputStream());
+        DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
 
         while (true) {
-            Socket socket = serverSocket.accept();
-            count += 1;
-            UserSocket userSocket = new UserSocket(count, socket);
-            userMap.put(count, userSocket);
-
-            print("Established a connection to host %s:%d\n",
-                    socket.getInetAddress(), socket.getPort(), socket.getInputStream());
-            new Thread(userSocket).start();
-        }
-    }
-
-    class UserSocket implements Runnable {
-
-        private DataInputStream in;
-        private DataOutputStream out;
-        private int userId;
-        private String userName;
-        private boolean connection;
-
-        public UserSocket(int userId, Socket socket) {
-            this.userId = userId;
-
-            try {
-                in = new DataInputStream(socket.getInputStream());
-                out = new DataOutputStream(socket.getOutputStream());
-                connection = true;
-            } catch (IOException e) {
-                e.printStackTrace();
-                connection = false;
-            }
-
-            try {
-                if (in != null) {
-                    byte[] buffer = new byte[1024];
-                    int size = in.readInt();
-                    String name = "";
-                    while(size > 0) {
-                        int len = in.read(buffer, 0, Math.min(size, buffer.length));
-                        name += new String(buffer, 0, len);
-                        size -= len;
-                    }
-                    this.userName = name;
-                }
-                sendOther("ID:" + this.userId + " " + this.userName + "send to you  ", true);
-                send("welcome to chat room "+  this.userName);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        public String receive() {
+            int size = in.readInt();
             String message = "";
-            byte[] buffer = new byte[1024];
-            try {
-                int size = in.readInt();
-                while(size > 0) {
-                    int len = in.read(buffer, 0, Math.min(size, buffer.length));
-                    message += new String(buffer, 0, len);
-                    size -= len;
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-                connection = false;
+            while (size > 0) {
+                int len = in.read(buffer, 0, Math.min(size, buffer.length));
+                message += new String(buffer, 0, len);
+                size -= len;
             }
-            return message;
-        }
 
-        public void send(String message) {
-            try {
-                out.writeInt(message.length());
-                out.write(message.getBytes(), 0, message.length());
-                out.flush();
-            } catch (IOException e) {
-                e.printStackTrace();
-                connection = false;
-            }
-        }
-
-        public void sendOther(String msg, boolean sys) {
-            if (msg.startsWith("@") && msg.contains(":")) {
-                Integer id = Integer.parseInt(msg.substring(1, msg.indexOf(":")));
-                String newMsg = msg.substring(msg.indexOf(":"));
-
-                UserSocket userSocket = userMap.get(id);
-                if (userSocket != null) {
-                    userSocket.send("ID:" + this.userId + " " + this.userName + " "+ "direct send you" + newMsg);
-                }
-            } else {
-                Collection<UserSocket> values = userMap.values();
-                for (UserSocket userSocket : values) {
-                    if (userSocket != this) {
-                        if (sys) {
-                            userSocket.send("System message:" + msg);
-                        } else {
-                            userSocket.send("ID:" + this.userId + " " + this.userName + msg);
-                        }
-                    }
-                }
-            }
-        }
-
-        @Override
-        public void run() {
-            while (connection) {
-                sendOther(receive(),false);
-            }
+            out.writeInt(message.length());
+            out.write(message.getBytes(), 0, message.length());
         }
     }
 
-    public static void main(String[] args) {
-        Server server = new Server();
-
-        try {
-            server.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws IOException {
+        int port = Integer.parseInt(args[0]);
+        new Server(port);
     }
 }
