bc9b2a27378ed4e7c0b72e03cdcf7d030142d783
WANGMinsi <90818691+WANGMinsi@users.noreply.github.com>
2022-11-24 22:52:10 +0800

    Add files via upload

---
+++ src/Test/SenderClient.java
@@ -0,0 +1,25 @@
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class SenderClient {
+    public SenderClient(String serverIP, int port) throws IOException {
+        Socket socket = new Socket(serverIP, port);
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String message = scanner.nextLine();
+            int size = message.length();
+            out.writeInt(size);
+            out.write(message.getBytes(), 0, size);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String serverIP = "192.168.68.82";
+        int port =12345;
+        new SenderClient(serverIP, port);
+    }
+}
---
+++ src/Test/Server.java
@@ -0,0 +1,46 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public Server(int port) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(port);
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            Thread t = new Thread(() -> {
+                try {
+                    serve(clientSocket);
+                } catch (IOException ex) {
+                    System.out.println("Connection drop!");
+                }
+            });
+            t.start();
+        }
+    }
+
+    private void serve(Socket clientSocket) throws IOException {
+        byte[] buffer = new byte[1024];
+        DataInputStream in = new DataInputStream(clientSocket.getInputStream());
+        DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+
+        while (true) {
+            int size = in.readInt();
+            String message = "";
+            while (size > 0) {
+                int len = in.read(buffer, 0, Math.min(size, buffer.length));
+                message += new String(buffer, 0, len);
+                size -= len;
+            }
+
+            out.writeInt(message.length());
+            out.write(message.getBytes(), 0, message.length());
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        int port = 12345;
+        new Server(port);
+    }
+}
